a1e12a0006daac97a305980911b4abe8162816f8
Xavier Paez <xavipaez2000@hotmail.com>
2023-06-01 00:40:41 -0500

    clase 3

--- app/src/main/java/com/example/dispositivosmoviles/MainActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/MainActivity.kt
@@ -1,11 +1,50 @@
-package com.example.dispositivosmoviles
+package com.example.dispositivosmoviles.ui.activies
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.dispositivosmoviles.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    //reescribir la funcion onCreate que hereda de AppCompactActivity
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        initClass()
+    }
+
+    private fun initClass() {
+        binding.boton1.setOnClickListener {
+            binding.textBuscar.text = "El codigo ejecuta correctamente"
+//Toast.makeText(this,
+// "Este es un ejemplo",
+// Toast.LENGTH_SHORT)
+// .show()
+
+            var f=Snackbar.make(binding.boton1,
+                "Este es otro mensaje",
+                Snackbar.LENGTH_LONG)
+//f.setBackgroundTint(R.color.black).show()
+            f.show()
+        }
+    }
+
+
+
 }
