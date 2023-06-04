45f41fabd7c95e2b1382839914e95068c13045b8
Xavier Paez <xavipaez2000@hotmail.com>
2023-06-04 18:06:01 -0500

    clase4

--- app/src/main/java/com/example/dispositivosmoviles/MainActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/MainActivity.kt
@@ -1,26 +1,28 @@
-package com.example.dispositivosmoviles.ui.activies
+package com.example.dispositivosmoviles.ui.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
+//esta clase hereda de AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var  binding : ActivityMainBinding
 
-    //reescribir la funcion onCreate que hereda de AppCompactActivity
-    @SuppressLint("ResourceAsColor")
+    //reescribir la funcion onCreate que hereda de  AppCompactActivity
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
     }
 
@@ -29,22 +31,31 @@ class MainActivity : AppCompatActivity() {
         initClass()
     }
 
-    private fun initClass() {
-        binding.boton1.setOnClickListener {
-            binding.textBuscar.text = "El codigo ejecuta correctamente"
-//Toast.makeText(this,
-// "Este es un ejemplo",
-// Toast.LENGTH_SHORT)
-// .show()
-
-            var f=Snackbar.make(binding.boton1,
-                "Este es otro mensaje",
-                Snackbar.LENGTH_LONG)
-//f.setBackgroundTint(R.color.black).show()
-            f.show()
-        }
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
+    private fun initClass() {
+        binding.boton1.setOnClickListener {
+            //binding.txtBuscar.text = "El codigo ejecuta correctamente"
+            //Toast.makeText(this,
+            //   "Este es un ejemplo",
+            //    Toast.LENGTH_SHORT)
+            //    .show()
 
+            /* var f=Snackbar.make(binding.boton1,
+                 "Este es otro mensaje",
+                 Snackbar.LENGTH_LONG)
+             //f.setBackgroundTint(R.color.black).show()
+             f.show()*/
+
+            var intent = Intent(this,
+                PrincipalActivity::class.java)
+            intent.putExtra("var1",binding.txtBuscar.text.toString()) //se pasa el nombre de la variable y valor
+            intent.putExtra("var2",11)
+            startActivity(intent)
+
+        }
+    }
 
 }
